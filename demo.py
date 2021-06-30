e92c8708f7dedf26e65b6a2ae9bfd4b000dff77c
dangtri2911@gmail.com <dangtri2911@gmail.com>
2021-06-30 20:42:29 +0700

    Demo

--- demo.py
+++ demo.py
@@ -2,6 +2,8 @@ import subprocess
 print("Demo abcs")
 
 subprocess.call(["ls","-l"])
+subprocess.call(["cd", "demo_testing_pipeline"])
+subprocess.call(["ls","-l"])
 # subprocess.call(["pytest"])
 
 # subprocess.run(["cd", "demo_testing_pipeline"])
