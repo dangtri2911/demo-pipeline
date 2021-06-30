9d23f1e9a7c22935055d30154be66ef8c5344832
dangtri2911@gmail.com <dangtri2911@gmail.com>
2021-06-30 20:38:17 +0700

    Demo

--- demo.py
+++ demo.py
@@ -1,5 +1,7 @@
 import subprocess
 print("Demo abcs")
+subprocess.run(["ls","-l"])
+subprocess.run(["pytest"])
 # subprocess.run(["cd", "demo_testing_pipeline"])
 # subprocess.run(["cd", "apitest"])
 # subprocess.run(["make", "run"])
